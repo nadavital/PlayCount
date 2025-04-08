8478782979f37a8bf22d2a95b03649d7c88b58e7
Nadav Avital <nadav.avital@icloud.com>
2025-04-08 10:30:23 -0700

    Initial Commit

---
+++ playCount/playCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  playCountApp.swift
+//  playCount
+//
+//  Created by Nadav Avital on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct playCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
